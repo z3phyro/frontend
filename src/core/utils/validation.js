6d6c404ed45f0d6fd809dd97f0f7aae2372fe9e9
Maykel Lopez <maykel.meneses89@gmail.com>
2020-07-22 16:37:18 +0200

    Moving validation to utils file

---
+++ src/core/utils/validation.js
@@ -0,0 +1,48 @@
+const getValidationMethods = (validationSchema, formErrors, setFormErrors, formState, setFormState, formBlur, setFormBlur) => {
+    const validate = async () => {
+        const isValid = await validationSchema.isValid(formState);
+
+        if (isValid) {
+            setFormErrors({});
+            return true;
+        }
+
+        try {
+            await validationSchema.validate(formState, {abortEarly: false})
+        } catch (e) {
+            const errors = {}
+            e.inner.forEach(error => {
+                errors[error.path] = error.message
+            })
+            setFormErrors(errors)
+        }
+
+        return false;
+    }
+    const onChange = (e) => {
+        setFormState({
+            ...formState,
+            [e.target.name]: e.target.value
+        });
+
+        validate();
+    }
+
+    const onBlur = (e) => {
+        setFormBlur({
+            ...formBlur,
+            [e.target.name]: true
+        });
+
+        validate();
+    }
+
+
+    return {
+        validate,
+        onChange,
+        onBlur
+    }
+}
+
+export default getValidationMethods;
--- src/views/login/login.js
+++ src/views/login/login.js
@@ -3,8 +3,9 @@ import "./login.scss";
 import textData from "../../content/auth.yaml";
 import * as yup from "yup"
 import InputGroup from "../../components/inputGroup/inputGroup";
+import getValidationMethods from "../../core/utils/validation";
 
-const schema = yup.object().shape({
+const validationSchema = yup.object().shape({
     email: yup.string("Please enter an email").email("Invalid email").required("Please enter an email"),
     password: yup.string("Password required").required("Password required")
 });
@@ -13,48 +14,12 @@ const Login = () => {
     const [formState, setFormState] = useState({
         email: "",
         password: "",
-    })
+    });
 
-    const [formErrors, setFormErrors] = useState({})
-    const [formBlur, setFormBlur] = useState({})
+    const [formErrors, setFormErrors] = useState({});
+    const [formBlur, setFormBlur] = useState({});
 
-    const validate = async () => {
-        const isValid = await schema.isValid(formState);
-
-        if (isValid) {
-            setFormErrors({});
-            return true;
-        }
-
-        try {
-            await schema.validate(formState, { abortEarly: false })
-        } catch (e) {
-            const errors = {}
-            e.inner.forEach(error => {
-                errors[error.path] = error.message
-            })
-            setFormErrors(errors)
-        }
-
-        return false;
-    }
-    const onChange = (e) => {
-        setFormState({
-            ...formState,
-            [e.target.name]: e.target.value
-        });
-
-        validate();
-    }
-
-    const onBlur = (e) => {
-        setFormBlur({
-            ...formBlur,
-            [e.target.name]: true
-        });
-
-        validate();
-    }
+    const {onChange, onBlur, validate} = getValidationMethods(validationSchema, formErrors, setFormErrors, formState, setFormState, formBlur, setFormBlur);
 
     const inputProps = {
         onBlur,
