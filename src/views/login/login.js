ecb86b1cb4808526802dc6cb0f78a2a4c4129623
Maykel Lopez <maykel.meneses89@gmail.com>
2020-07-22 14:48:30 +0200

    Adding validation to login

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,8 +1,60 @@
-import React from "react";
+import React, {useState} from "react";
 import "./login.scss";
 import textData from "../../content/auth.yaml";
+import * as yup from "yup"
+
+const schema = yup.object().shape({
+    email: yup.string("Please enter an email").email("Invalid email").required("Please enter an email"),
+    password: yup.string("Password required").required("Password required")
+});
 
 const Login = () => {
+    const [formState, setFormState] = useState({
+        email: "",
+        password: "",
+    })
+
+    const [formErrors, setFormErrors] = useState({})
+    const [formBlur, setFormBlur] = useState({})
+
+    const validate = async () => {
+        const isValid = await schema.isValid(formState);
+
+        if (isValid) {
+            setFormErrors({});
+            return true;
+        }
+
+        try {
+            await schema.validate(formState, { abortEarly: false })
+        } catch (e) {
+            const errors = {}
+            e.inner.forEach(error => {
+                errors[error.path] = error.message
+            })
+            setFormErrors(errors)
+        }
+
+        return false;
+    }
+    const onChange = (e) => {
+        setFormState({
+            ...formState,
+            [e.target.name]: e.target.value
+        });
+
+        validate();
+    }
+
+    const onBlur = (e) => {
+        setFormBlur({
+            ...formBlur,
+            [e.target.name]: true
+        });
+
+        validate();
+    }
+
     return <div className="LoginContainer">
         <div className="LoginBox">
             <div className="Title">{textData.login.title}</div>
@@ -13,18 +65,20 @@ const Login = () => {
                     <div className="Label">
                         {textData.login.email}
                     </div>
-                    <input type="text" className="Input"/>
+                    <input name="email" type="text" className={`Input ${formErrors["email"] ? 'Error' : ''}`} onBlur={onBlur} onChange={onChange}/>
+                    {formErrors["email"] && formBlur["email"] && <div className="ErrorText">{formErrors["email"]}</div>}
                 </div>
-                <div className="InputGroup Bottom">
+                <div className="InputGroup">
                     <div className="Label">
                         {textData.login.password}
                     </div>
-                    <input type="text" className="Input"/>
+                    <input name="password" type="text" className={`Input Bottom ${formErrors["password"] ? 'Error' : ''}`} onBlur={onBlur} onChange={onChange}/>
+                    {formErrors["password"] && formBlur["password"] && <div className="ErrorText">{formErrors["password"]}</div>}
                 </div>
                 <a href="#" className="ForgotPassword">{textData.login.forgotPassword}</a>
             </div>
 
-            <div className="Button">{textData.login.button}</div>
+            <div className="Button" onClick={validate}>{textData.login.button}</div>
             <a href="#" className="NoAccount">{textData.login.noAccount}</a>
         </div>
     </div>
